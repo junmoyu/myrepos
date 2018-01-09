0c045bd84ba3d49ea1ba359df0b8a09f1050d355
junmoyu <guoleiscu@163.com>
2018-01-09 15:13:38 +0800

    mytestddddd

---
+++ maventest/src/main/java/com/study/entity/Emp.java
@@ -0,0 +1,15 @@
+package com.study.entity;
+
+public class Emp {
+	String empName;
+	public Emp() {
+		// TODO Auto-generated constructor stub
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	
+}
